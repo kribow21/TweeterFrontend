2ca0d9aa709dfcf4ddbe1afeca9a6bc691c1b2e6
kribow21 <kristininnotech@gmail.com>
2021-08-25 13:39:52 -0600

    added ability to delete user on their profile after entering in their password that matches backends

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,17 +2,20 @@
     <div>
         <UserProfile/>
         <ProfileEditComp/>
+        <ProfileDeleteComp/>
     </div>
 </template>
 
 <script>
 import UserProfile from '../components/UserProfile.vue'
 import ProfileEditComp from '../components/ProfileEditComp.vue'
+import ProfileDeleteComp from '../components/ProfileDeleteComp.vue'
     export default {
         name : 'Profile',
         components: {
             UserProfile,
-            ProfileEditComp
+            ProfileEditComp,
+            ProfileDeleteComp
         },
     }
 </script>
