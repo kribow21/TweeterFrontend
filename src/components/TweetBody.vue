424e8da987766b863ffc5d0c32c2d395777d93ab
kribow21 <kristininnotech@gmail.com>
2021-08-26 20:36:48 -0600

    able to edit my or users logged in tweets on profile

--- src/components/TweetBody.vue
+++ src/components/TweetBody.vue
@@ -1,56 +1,77 @@
 <template>
     <div>
         <v-card
-    class="mx-auto"
-    color="primary"
-    dark
-    max-width="100%"
-  >
-    <v-card-title>
-      <v-icon
-        large
-        left
-      >
-        mdi-twitter
-      </v-icon>
-      <span class="text-h6 font-weight-light">Tweeter</span>
-    </v-card-title>
-
-    <v-card-text class="text-h5 font-weight-bold">
-        {{content}}
-    </v-card-text>
-
-    <v-card-actions>
-      <v-list-item class="grow">
-        <v-list-item-avatar color="grey darken-3">
-          <v-img
-            class="elevation-6"
-            alt=""
-            src="imageUrl"
-          ></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>{{username}}</v-list-item-title>
-        </v-list-item-content>
-
-        <v-row
-          align="center"
-          justify="end"
+        class="mx-auto"
+        color="primary"
+        dark
+        max-width="100%"
         >
-          <v-icon class="mr-1">
-            mdi-heart
-          </v-icon>
-          <span class="subheading mr-2">256</span>
-          <span class="mr-1">·</span>
-        </v-row>
-      </v-list-item>
-    </v-card-actions>
-  </v-card>
+        <v-card-title>
+        <v-icon
+            large
+            left
+            >
+            mdi-twitter
+            </v-icon>
+        <span class="text-h6 font-weight-light">Tweeter</span>
+        </v-card-title>
+
+        <v-card-text class="text-h5 font-weight-bold">
+            {{content}}
+        </v-card-text>
+
+        <v-card-actions>
+        <v-list-item class="grow">
+            <v-list-item-avatar color="grey darken-3">
+                <v-img
+                class="elevation-6"
+                alt=""
+                src="imageUrl"
+                ></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+                <v-list-item-title>{{username}}</v-list-item-title>
+            </v-list-item-content>
+
+            <v-row
+                align="center"
+                justify="end"
+            >
+                <v-icon @click="clickToEdit" class="mr-1">
+                mdi-comment-edit-outline
+                </v-icon>
+                        <div :class="{EditForm : isForm}">
+                        <v-col
+                            cols="12"
+                            sm="10"
+                        >
+                        <v-text-field
+                            label="edited tweet"
+                            v-model="editedContent"
+                            outlined
+                        ></v-text-field>
+                        <v-btn
+                                @click="editTweet"
+                                color="primary"
+                                elevation="2"
+                                raised
+                            >Apply</v-btn>
+                        </v-col>
+                        </div>
+                <v-icon class="mr-1">
+                mdi-comment-remove-outline
+                </v-icon>
+            </v-row>
+            </v-list-item>
+        </v-card-actions>
+    </v-card>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import cookies from "vue-cookies"
     export default {
         name : 'TweetBody',
         props : {
@@ -60,10 +81,50 @@
             createdAt: String,
             tweetId: Number,
             imageUrl: String
+        },
+        methods: {
+            clickToEdit(){
+                if (this.isForm == true){
+                    this.isForm = false
+                }else if(this.isForm == false){
+                    this.isForm = true
+                }
+            },
+            editTweet() {
+                axios.request({
+                    url : "https://tweeterest.ml/api/tweets",
+                    method : "PATCH",
+                    headers : {
+                        'X-Api-Key' : process.env.VUE_APP_API_KEY,
+                        'Content-Type': 'application/json'
+                    },
+                    data: {
+                        "loginToken" : cookies.get('loginToken'),
+                        "tweetId": this.tweetId,
+                        "content": this.editedContent
+                    }
+                }).then((response) => {
+                    console.log(response); 
+
+                }).catch((error) => {
+                    console.error("There was an error" +error);
+                })
+            }
+        },
+        data() {
+            return {
+                isForm: true,
+                editedContent: "",
+            }
         }
     }
 </script>
 
 <style scoped>
-
+.mx-auto{
+    margin-top: 3%;
+}
+.EditForm{
+    display: none;
+}
 </style>
