fc16c87a8416fa15c25b08e8966bddf24b7e2d46
kribow21 <kristininnotech@gmail.com>
2021-08-30 16:30:32 -0600

    figured out function problem on follow/unfollow buttons..now to figure out how to save the state of that button

--- src/components/OtherUserProfile.vue
+++ src/components/OtherUserProfile.vue
@@ -10,18 +10,18 @@
                 <p>{{proBio}}</p>
             <v-btn
                 v-if="state === 'follow'"
-                @click="followOtherUser; changeState('unfollow')"
+                @click="followOtherUser(); changeState('unfollow')"
                 color="accent"
                 elevation="2"
                 raised
             >Follow </v-btn>
             <v-btn
                 v-else
-                @click="unfollowOtherUser; changeState('follow')"
+                @click="unfollowOtherUser(); changeState('follow')"
                 color="accent"
                 elevation="2"
                 raised
-            >Unfollow something</v-btn>
+            >Unfollow</v-btn>
             </v-col>
         </v-row>
     </v-container>
@@ -29,7 +29,7 @@
 
 <script>
 import axios from "axios";
-// import cookies from "vue-cookies"
+import cookies from "vue-cookies"
     export default {
         name : 'OtherUserProfile',
         data() {
@@ -38,6 +38,7 @@ import axios from "axios";
                 userPic: "",
                 proBio: "",
                 username: "",
+                toggle_one: 0
             }
         },
         mounted () {
@@ -74,43 +75,43 @@ import axios from "axios";
             },
         followOtherUser(){
             console.log("follow");
-            // axios.request({
-            //         url : "https://tweeterest.ml/api/follows",
-            //         method : "POST",
-            //         headers : {
-            //             'X-Api-Key' : process.env.VUE_APP_API_KEY,
-            //             'Content-Type': 'application/json'
-            //         },
-            //         data : {
-            //             "loginToken": cookies.get('loginToken'),
-            //             "followId" : this.userId
-            //         }
-            //     }).then((response) => {
-            //         console.log(response);
+            axios.request({
+                    url : "https://tweeterest.ml/api/follows",
+                    method : "POST",
+                    headers : {
+                        'X-Api-Key' : process.env.VUE_APP_API_KEY,
+                        'Content-Type': 'application/json'
+                    },
+                    data : {
+                        "loginToken": cookies.get('loginToken'),
+                        "followId" : this.userId
+                    }
+                }).then((response) => {
+                    console.log(response);
 
-            //     }).catch((error) => {
-            //         console.error("There was an error" +error);
-            //     })
+                }).catch((error) => {
+                    console.error("There was an error" +error);
+                })
         },
         unfollowOtherUser(){
             console.log("unfollow");
-                // axios.request({
-                //     url : "https://tweeterest.ml/api/follows",
-                //     method : "DELETE",
-                //     headers : {
-                //         'X-Api-Key' : process.env.VUE_APP_API_KEY,
-                //         'Content-Type': 'application/json'
-                //     },
-                //     data : {
-                //         "loginToken": cookies.get('loginToken'),
-                //         "followId" : this.userId
-                //     }
-                // }).then((response) => {
-                //     console.log(response);
+                axios.request({
+                    url : "https://tweeterest.ml/api/follows",
+                    method : "DELETE",
+                    headers : {
+                        'X-Api-Key' : process.env.VUE_APP_API_KEY,
+                        'Content-Type': 'application/json'
+                    },
+                    data : {
+                        "loginToken": cookies.get('loginToken'),
+                        "followId" : this.userId
+                    }
+                }).then((response) => {
+                    console.log(response);
 
-                // }).catch((error) => {
-                //     console.error("There was an error" +error);
-                // })
+                }).catch((error) => {
+                    console.error("There was an error" +error);
+                })
         }
         }
     }
